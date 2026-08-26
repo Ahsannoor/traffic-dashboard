e2c7e8abe44cf8c3f9fbf9187186f1c7e1521784
Ahsannoor <ahsannoor1010@gmail.com>
2026-08-26 20:46:48 +0400

    module setup for traffic including controller and service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrafficModule } from './traffic/traffic.module';
 
 @Module({
-  imports: [],
+  imports: [TrafficModule],
   controllers: [AppController],
   providers: [AppService],
 })
