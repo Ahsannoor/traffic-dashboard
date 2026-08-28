aa7aa05c690828f63cddff60b3e71bd6b20c3cbb
Ahsannoor <ahsannoor1010@gmail.com>
2026-08-28 19:12:22 +0400

    handle cors for backend through env, added example env

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,13 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  let port = process.env.PORT ?? 3000
-  console.log(`Application running on ${port}`)
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_ORIGIN ?? "http://localhost:3000",
+    origin: (process.env.CORS_ORIGIN || 'http://localhost:3000').split(','),
     credentials: true,
   });
   await app.listen(port);
+  console.log(`Application running on ${port}`);
 }
 bootstrap();
