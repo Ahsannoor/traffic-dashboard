c7a53ffd3c86886bc28392c04a90183c5f71a431
Ahsannoor <ahsannoor1010@gmail.com>
2026-08-29 11:08:56 +0400

    fix the default port to 4000

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: (process.env.CORS_ORIGIN || 'http://localhost:3000').split(','),
